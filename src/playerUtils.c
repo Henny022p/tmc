c7aa2d411fdceee9675a12e864b50bd52755ed21
Henny022p <info@henny022.de>
2022-04-22 07:18:13 +0200

    skip intro cutscene

--- src/playerUtils.c
+++ src/playerUtils.c
@@ -3844,11 +3844,17 @@ u32 FinalizeSave(void) {
         gSave.stats.maxHealth = 24;
         gSave.saved_status.area_next = 0x22;
         gSave.saved_status.room_next = 0x15; // links room
-        gSave.saved_status.start_anim = 0;
+        gSave.saved_status.start_anim = Direction8ToAnimationState(DirectionSouth);
         gSave.saved_status.spawn_type = PL_SPAWN_DEFAULT;
         gSave.saved_status.layer = 1;
-        gSave.saved_status.start_pos_x = 0x90;
+        gSave.saved_status.start_pos_x = 0x78;
         gSave.saved_status.start_pos_y = 0x38;
+
+        SetGlobalFlag(0x246);
+        {
+            extern u32 gRand;
+            gRand = 0x248AA711;
+        }
     }
     if (gSave.name[0] == 0) {
         MemCopy(gUnk_0811E470, &gSave.name, FILENAME_LENGTH - 1);
